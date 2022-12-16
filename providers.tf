dceb15bd7d41fc3ab4e7bdce40d39e2b4819dfd2
Bhavana <bspalande@gmail.com>
2022-12-16 09:21:59 +0530

    updating jenkins file

--- providers.tf
+++ providers.tf
@@ -1,13 +1,22 @@
 terraform {
+  required_version = ">=0.12"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = "~>2.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
+    }
+    tls = {
+      source = "hashicorp/tls"
+      version = "~>4.0"
     }
   }
 }
 
-# Configure the Microsoft Azure Provider
 provider "azurerm" {
   features {}
 }
